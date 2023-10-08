a9aade45cd7f06e21c8e1b2e0a2bedb6b40fe1f6
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-10-08 15:12:12 +0200

    [NEW] Creacion de sincronía para recibir el dato

---
+++ index.mjs
@@ -0,0 +1,17 @@
+const getAllPotions = async () => {
+    return fetch('https://raw.githubusercontent.com/zsiciarz/skyrim-alchemy-toolbox/master/data/ingredients.json')
+    .then(response => response.json())
+}
+
+
+
+const fetchAsyncPotions = async() => {
+    try {
+        const getPotions = await getAllPotions()
+        console.log(getPotions);
+    } catch (error){
+        console.log(error.message)
+    }
+}
+
+fetchAsyncPotions();
