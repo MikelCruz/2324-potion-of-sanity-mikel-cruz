84f9951c13c1f33ba95e8be65e359e1c7a3334e4
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-10-16 09:30:50 +0200

    [NEW] Creacion de clase Ingredient

---
+++ ingredient.mjs
@@ -0,0 +1,20 @@
+import Effect from "./effect.mjs";
+
+export default class  
+{
+    constructor(name, effects, value, weight ){
+        this.name       = name;     // BD
+        this.effects    = effects;  // exportado de effects
+        this.value      = value;    // BD
+        this.weight     = weight;   // BD
+    }
+
+    static from(name, effects, value, weight){
+        return new Ingredient (
+            name,
+            effects.map (effect => Effect.from(effect)),
+            value,
+            weight
+            );
+    }
+}
